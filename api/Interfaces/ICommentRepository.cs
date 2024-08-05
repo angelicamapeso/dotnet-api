0418cf094a0baff5c9d3d3cf87b15acc7a4fe773
Angelica Mapeso <angelica.mapeso@gmail.com>
2024-08-05 11:27:17 -0400

    add comment udpate

--- api/Interfaces/ICommentRepository.cs
+++ api/Interfaces/ICommentRepository.cs
@@ -11,5 +11,6 @@ namespace api.Interfaces
         Task<List<Comment>> GetAllAsync();  
         Task<Comment?> GetByIdAsync(int id);
         Task<Comment> CreateAsync(Comment comment);
+        Task<Comment?> UpdateAsync(int id, Comment commentModel);
     }
 }
